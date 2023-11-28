95bcff248ce70fd9d1bee7d13ca3ba46e690bb9c
Vlad <Sp0ngbik@gmail.com>
2023-11-28 21:16:56 +0300

    add RTK

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,13 +3,14 @@ import {tasks_reducer} from "./reducers/tasks_reducer";
 import {todoList_reducer} from "./reducers/todoList_reducer";
 import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {app_reducer} from "./reducers/app_reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
     tasks_reducer: tasks_reducer,
     todoList_reducer: todoList_reducer,
     app_reducer: app_reducer
 })
-
+// const rootReducer = configureStore({})
 // type T_MainAppAction = T_MainTL | T_MainTasks
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 export type RootState = ReturnType<typeof rootReducer>
